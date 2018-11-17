b23088ac896389b7f78720f279e189a5ed2058c8
edu san <33841139+econdepe@users.noreply.github.com>
2018-11-17 22:30:28 +0100

    Update routes.js
    Outdated version in this github. Using the one from deadmanBTC github

--- frontend/api/routes/routes.js
+++ frontend/api/routes/routes.js
@@ -1,23 +1,36 @@
-var appRouter = function (app) {
-    app.get("/", function(req, res) {
-      res.status(200).send("Welcome to our restful API");
+const appRouter = function (app) {
+    app.get("/", function (req, res) {
+        res.status(200).send("Welcome to our restful API");
     });
 
-    //receive information from form
-    app.post("/register", function(req, res) {
+    app.post("/register", function (req, res) {
 
         //send to ledger
-        var publicKey = req.body.publickey;
-        var timeperiod = req.body.timeperiod;
-        var currenthash = req.body.currenthash;
-        var password = req.body.password;
+        //TODO double the number below after the ledger app fix in make
+        let publicKey = req.body.publickey.padStart(33, "0");
+        let timeperiod = req.body.timeperiod.padStart(4, "0");
+        let currenthash = req.body.currenthash.padStart(32, "0");
+        let password = req.body.password.padStart(64, "0");
+
+        app.registerOnLedger(publicKey, timeperiod, currenthash, password)
+            .then(result => {
+                res.status(200).send();
+            })
+            .catch(err => {
+                res.status(403).send();
+            });
 
     });
 
-    app.get("/check", function(req, res) {
-        res.status(200).send(JSON.stringify({privatekey: 'E9873D79C6D87DC0FB6A5778633389F4453213303DA61F20BD67FC233AA33262', status: 0}));
-
+    app.get("/check", function (req, res) {
+        app.check()
+            .then(result => {
+                res.status(200).send("{\"privateKey\": \"\"}");
+            })
+            .catch(err => {
+                res.status(400).send("{\"privateKey\": \""+err+ "\"}");
+            });
     });
-  };
+};
 
-  module.exports = appRouter;
+module.exports = appRouter;
